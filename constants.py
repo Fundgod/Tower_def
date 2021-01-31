14a24ef4a86b266ffaa59d6c91991d92bc4292cd
Fundgod <44843271+Fundgod@users.noreply.github.com>
2021-01-31 11:10:35 +0700

    constants were returned

--- constants.py
+++ constants.py
@@ -12,32 +12,58 @@ CRYSTAL_GOLEM = 'crystal_golem'
 MOBS = (MASK, SKILLET, STONE_GOLEM, BOAR_WARRIOR, HORNY_DOG, CRYSTAL_GOLEM)
 CACHE_VELOCITY = 1 / 30
 MAINTOWERS_POSITIONS = {
-    1: (20, 670),  # Для уровня 1
-    2: (-10, 340),  # Для уровня 2
-    3: (-10, 353)  # Для уровня 3
+    1: (20, 670),  # Для первого уровня
+    2: (-10, 340),  # Для второго уровня
+    3: (-10, 353)  # Для третьего уровня
 }
 SPAWN_DATA = {
     1: (
         (1, 0, SKILLET),
+        (1, 0, SKILLET),
+        (1, 0, SKILLET),
+        (12, 0, SKILLET),
+        (1, 0, SKILLET),
+        (1, 0, SKILLET),
+        (1, 0, SKILLET),
+        (1, 0, CRYSTAL_GOLEM),
+        (1, 0, SKILLET),
+        (1, 0, SKILLET),
         (2, 0, CRYSTAL_GOLEM),
-        (1, 0, HORNY_DOG),
-        (1, 0, BOAR_WARRIOR),
+        (4, 0, HORNY_DOG),
+        (12, 0, BOAR_WARRIOR),
         (2, 0, MASK),
         (3, 0, STONE_GOLEM),
         (1, 0, HORNY_DOG),
+        (1, 0, HORNY_DOG),
+        (1, 0, CRYSTAL_GOLEM),
         (2, 0, SKILLET),
         (3, 0, BOAR_WARRIOR)
     ),
     2: (
         (1, 0, STONE_GOLEM),
+        (1, 0, STONE_GOLEM),
+        (1, 0, STONE_GOLEM),
+        (1, 0, STONE_GOLEM),
+        (1, 0, STONE_GOLEM),
         (2, 1, CRYSTAL_GOLEM),
-        (3, 0, HORNY_DOG),
-        (1, 1, BOAR_WARRIOR),
+        (15, 1, BOAR_WARRIOR),
         (2, 0, MASK),
         (3, 1, SKILLET),
-        (1, 0, HORNY_DOG),
         (2, 1, SKILLET),
-        (3, 0, BOAR_WARRIOR)
+        (0, 1, SKILLET),
+        (1, 1, SKILLET),
+        (1, 1, SKILLET),
+        (1, 1, SKILLET),
+        (1, 1, SKILLET),
+        (1, 1, SKILLET),
+        (1, 1, SKILLET),
+        (16, 1, SKILLET),
+        (1, 0, BOAR_WARRIOR),
+        (1, 0, BOAR_WARRIOR),
+        (1, 0, HORNY_DOG),
+        (1, 1, STONE_GOLEM),
+        (1, 1, STONE_GOLEM),
+        (1, 1, STONE_GOLEM),
     ),
     3: (
         (1, 0, STONE_GOLEM),
@@ -46,8 +72,13 @@ SPAWN_DATA = {
         (1, 1, BOAR_WARRIOR),
         (2, 0, MASK),
         (3, 1, SKILLET),
+        (0.5, 0, CRYSTAL_GOLEM),
+        (0, 1, STONE_GOLEM),
+        (0.5, 0, CRYSTAL_GOLEM),
+        (0, 1, STONE_GOLEM),
+        (0.5, 0, CRYSTAL_GOLEM),
+        (0, 1, STONE_GOLEM),
         (1, 0, HORNY_DOG),
-        (2, 1, SKILLET),
-        (3, 0, BOAR_WARRIOR)
+        (2, 1, SKILLET)
     )
 }
